13a0f95f2d4ccfb0938efa5af90573010401e096
jiaming <jiaminglim0828@gmail.com>
2023-12-10 16:10:25 +0800

    update images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col px-6 container mx-auto py-3">
+  <div class="flex flex-col px-2 container mx-auto py-3">
     <div class="flex flex-wrap">
       <div class="grid grid-cols-2 lg:w-1/2 w-96 bg-orange-600">
         <div class="py-1 pl-2">WHAT'S ON TONIGHT</div>
@@ -17,34 +17,53 @@
         <div>GO</div>
       </div>
     </div>
-    <div class="flex flex-col md:flex-row flex-grow gap-8 mt-8">
-      <div class="flex flex-col bg-black">
-        <div class="h-96 w-48">a</div>
-      </div>
-      <div class="flex flex-col gap-4 w-full">
-        <div class="w-full grid grid-cols-3">
-          <div class="col-span-2 border">content</div>
-          <div class="border">content</div>
-        </div>
-        <div class="w-full grid grid-cols-3">
-          <div class="border">content</div>
-          <div class="border">content</div>
-          <div class="border">content</div>
-        </div>
-        <div class="w-full grid grid-cols-3">
-          <div class="border">content</div>
-          <div class="border">content</div>
-          <div class="border">content</div>
-        </div>
-        <div>PRIVACY POLICY</div>
-        <div class="flex gap-2 flex-wrap">
 
-          <div>TRADEMARK INFORMATION</div>
-          <div>TERMS OF USE</div>
-          <div>PARENT GUIDE</div>
-          <div>TV PARENTAL GUIDELINES</div>
+    <div class="flex flex-col md:grid grid-cols-9 gap-4 mt-8">
+
+      <div class="flex flex-col w-full md:w-fit gap-4 md:col-span-2">
+          <div class="bg-black p-2">
+            <img src="@/assets/a.jpg" />
+          </div>
+          <div class="bg-black p-2">
+            <img src="@/assets/a.jpg" />
+          </div>
+
+        <div class="bg-black p-2">
+          <img src="@/assets/b.jpeg" class="w-full  md:h-52" />
         </div>
-        <div>USE OF THIS SITE SIGNIFIES TOUR AGREEMENT TO THE TERMS OF USE</div>
+        <div class="bg-black p-2">
+          <img src="@/assets/c.png" class="w-full md:h-44" />
+        </div>
+      </div>
+
+      <div class="flex flex-col gap-3 w-full md:col-span-7">
+        <div class="w-full grid grid-cols-5 gap-3">
+          <div class="col-span-4 border">
+            <img src="@/assets/d.png" class="w-full" />
+          </div>
+          <img src="@/assets/e.jpeg" class="w-full h-full" />
+        </div>
+        <div class="w-full grid grid-cols-5 gap-3">
+          <div class="border"><img src="@/assets/e.jpeg" class="w-full h-52" /></div>
+          <div class="border col-span-3"><img src="@/assets/e.jpeg" class="w-full h-52" /></div>
+          <div class="border"><img src="@/assets/e.jpeg" class="w-full h-52" /></div>
+        </div>
+        <div class="w-full grid grid-cols-5 gap-3">
+          <div class="border"><img src="@/assets/cn-black.png" class="w-full h-28" /></div>
+          <div class="border col-span-3"><img src="@/assets/cn-black.png" class="w-full h-28" /></div>
+          <div class="border"><img src="@/assets/cn-black.png" class="w-full h-28" /></div>
+        </div>
+        <div class="flex flex-col text-sm">
+
+          <div> > PRIVACY POLICY</div>
+          <div class="flex gap-1.5 flex-wrap">
+            <div>> TRADEMARK INFORMATION</div>
+            <div>> TERMS OF USE</div>
+            <div>> PARENT GUIDE</div>
+            <div>> TV PARENTAL GUIDELINES</div>
+          </div>
+          <div>USE OF THIS SITE SIGNIFIES YOUR AGREEMENT TO THE TERMS OF USE. @2020 CARTOON NETWORK. A TIME WARNER COMPANY.</div>
+        </div>
       </div>
     </div>
   </div>
